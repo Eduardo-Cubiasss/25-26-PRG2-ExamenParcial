582141f29313cb4347a7ab7f5d839ac3e3953038
eduardo.cubias <eduardo.cubiasm@gmail.com>
2026-03-27 00:27:11 +0100

    feat: add class historialPrestamo and methods public

---
+++ entregas/cubiasMoran/src/HistorialPrestamo.java
@@ -0,0 +1,19 @@
+class HistorialPrestamo {
+    private Prestamo[] prestamos;
+
+    public HistorialPrestamo(Prestamo[] prestamos) {
+        this.prestamos = prestamos;
+    }
+
+    public void agregarPrestamo(Prestamo nuevoPrestamo) {
+        Prestamo[] nuevoArreglo = new Prestamo[this.prestamos.length + 1];
+        
+        for (int i = 0; i < this.prestamos.length; i++) {
+            nuevoArreglo[i] = this.prestamos[i];
+        }
+        
+        nuevoArreglo[nuevoArreglo.length - 1] = nuevoPrestamo;
+        this.prestamos = nuevoArreglo;
+    }
+
+}
